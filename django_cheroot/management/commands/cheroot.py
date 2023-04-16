95061b572f8ea35a49a053a1f8faadbbf2ad6340
modbender <rameshmamathayashas@gmail.com>
2023-04-16 15:35:20 +0530

    changes to arguments, update README

--- django_cheroot/management/commands/cheroot.py
+++ django_cheroot/management/commands/cheroot.py
@@ -8,7 +8,7 @@ def run_cheroot_server(
     port: int = 8000,
     numthreads: int = 30,
     max_threads: int = 40,
-    request_queue_size: int = 20,
+    connections: int = 20,
 ):
     application = get_wsgi_application()
     addr = host, port
@@ -16,7 +16,7 @@ def run_cheroot_server(
         addr, application,
         numthreads=numthreads,  # minimum number of threads to keep in thread pool
         max=max_threads,  # maximum number of threads.
-        request_queue_size=request_queue_size
+        request_queue_size=connections
     )
     server.start()
 
@@ -26,25 +26,25 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('-p', '--port', type=int,
-                            help='Port to run server on')
-        parser.add_argument('-ip', '--host_ip', type=str, help='Host IP')
-        parser.add_argument('-t', '--numthreads', type=int,
+                            help='Server Port')
+        parser.add_argument('-h', '--host', type=str, help='Host IP')
+        parser.add_argument('-t', '--minthreads', type=int,
                             help='Min threads in Thread Pool for CherryPy Server')
         parser.add_argument('-w', '--maxthreads', type=int,
                             help='Max worker threads')
-        parser.add_argument('-c', '--request_queue_size',
+        parser.add_argument('-c', '--connections',
                             type=int, help='Max queued connections')
 
     def handle(self, *args, **options):
         port = options['port'] or 8000
-        host = options['host_ip'] or '127.0.0.1'
-        numthreads = options['numthreads'] or 20
+        host = options['host'] or '127.0.0.1'
+        numthreads = options['minthreads'] or 20
         max_threads = options['maxthreads'] or 40
-        request_queue_size = options['request_queue_size'] or 20
+        connections = options['connections'] or 20
         run_cheroot_server(
             port=port,
             host=host,
             numthreads=numthreads,
             max_threads=max_threads,
-            request_queue_size=request_queue_size
+            connections=connections
         )
